5cfa59fbadb9d49f35ebbf2319d18ac0fc22b3a4
Pierre Bermudez <bermudezpierre@gmail.com>
2025-01-16 16:48:56 +0100

    Add random function

--- projects/starwars/index.js
+++ projects/starwars/index.js
@@ -6,7 +6,7 @@ const random = document.getElementById('random');
 // Création des éléments HTML
 const infos = document.querySelector('.infos');
 
-// Buttons
+// Buttons de 1 à 10
 buttons.forEach((btn) => {
   // Fetch API avec async await
   btn.addEventListener('click', async () => {
@@ -43,6 +43,7 @@ buttons.forEach((btn) => {
 
 //Random button
 random.addEventListener('click', async () => {
+  infos.innerHTML = '';
   const num = buttons[Math.floor(Math.random() * buttons.length)].textContent;
   const urlAPI = `https://swapi.dev/api/people/${num}`;
   try {
@@ -71,5 +72,37 @@ random.addEventListener('click', async () => {
     console.log('Fetch error:', error.message);
   } finally {
     console.log('Fetch finished');
+  }
+});
+
+// Button All
+allBtn.addEventListener('click', async () => {
+  infos.innerHTML = '';
+  for (let number = 1; number <= 10; number++) {
+    const urlAPI = `https://swapi.dev/api/people/${number}`;
+    try {
+      const response = await fetch(urlAPI);
+      const perso = await response.json();
+      console.log(perso);
+
+      // Création des élément du DOM
+      const personnage = document.createElement('div');
+      const nomElement = document.createElement('h2');
+      const tailleElement = document.createElement('p');
+      const poidsElement = document.createElement('p');
+
+      // Mise à jour du contenu des éléments HTML
+      nomElement.textContent = `Nom: ${perso.name}`;
+      tailleElement.textContent = `Taille: ${perso.height} cm`;
+      poidsElement.textContent = `Poids: ${perso.mass} kg`;
+
+       // Ajout des éléments au DOM
+      infos.appendChild(personnage);
+      infos.appendChild(nomElement);
+      infos.appendChild(tailleElement);
+      infos.appendChild(poidsElement);
+    } catch (error) {
+      console.log('Fetch error:', error.message);
+    }
   }
 });
