73b996156a3d9a65ab7b77ca8609eee43b38dbe8
Pierre Bermudez <bermudezpierre@gmail.com>
2025-01-16 18:06:29 +0100

    Add pokemon fetch name, id, lang

---
+++ projects/pokemon/index.js
@@ -0,0 +1,35 @@
+
+
+async function getPokemons(number,lang) {
+  try {
+    // URL de la génération de pokemons
+    const urlAPI = `https://pokeapi.co/api/v2/generation/${number}`;
+    console.log(urlAPI);
+
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+    const pokemons = data.pokemon_species;
+    console.log(pokemons);
+
+    pokemons.forEach(pokemon => {
+      const pokeName = pokemon.name;
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data.id, data.sprites.front_shiny)
+        });
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}`)
+        .then(response => response.json())
+        .then(data => {
+          const pokeName = data.names.filter(langue => langue.language.name === lang)[0].name;
+          console.log(pokeName);
+        });
+    });
+  } catch (error) {
+    console.log('Fetch error:', error.message);
+  } finally {
+    console.log('Fetch finished');
+  }
+}
+
+getPokemons(1, 'fr');
